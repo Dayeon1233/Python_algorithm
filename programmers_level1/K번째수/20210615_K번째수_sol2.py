5dda5b0e5f2e03bc2f6f4d994e56763a3206fa55
dayeon <swdy1233@gmail.com>
2021-06-15 23:29:38 +0900

    20210615_K번째수

--- programmers_level1/K번째수/20210615_K번째수_sol2.py
+++ programmers_level1/K번째수/20210615_K번째수_sol2.py
@@ -1,11 +1,8 @@
 def solution(array, commands):
-    answer = []
-    for i in commands :
-        array2 = array[(i[0])-1 : i[1]]
-        array2.sort()
-        answer.append(array2[(i[2])-1])
-    return answer
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
 
+def solution(array, commands):
+    return [sorted(array[i-1:j])[k-1] for i,j,k in commands]
 
 
 if __name__ == '__main__':
