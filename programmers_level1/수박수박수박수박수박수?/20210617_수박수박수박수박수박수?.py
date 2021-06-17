acf7ee526de8a1456d3e1296f96aaf194d007e17
dayeon <swdy1233@gmail.com>
2021-06-17 23:49:59 +0900

    20210617_수박수박수박수박수박수?

---
+++ programmers_level1/수박수박수박수박수박수?/20210617_수박수박수박수박수박수?.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = ''
+    if n % 2 == 0 :
+        answer += '수박' * (n//2)
+    else :
+        answer += '수박' * (n//2)
+        answer += '수'
+    return answer
+
+if __name__ == '__main__':
+    n = 4
+    print(solution(n))
+
+
