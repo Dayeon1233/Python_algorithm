35dcb79442e3d1e8b0fb54294992b25383fbed92
dayeon <swdy1233@gmail.com>
2021-06-27 00:03:28 +0900

    20210627_더맵게

---
+++ programmers_level2/더맵게/20210626_더맵게.py
@@ -0,0 +1,30 @@
+#풀다가 그만두고다른풀이로,,
+
+def solution(s):#baabaa
+    index = 0
+    lists = []
+    lists = list(s)
+    initlenS = len(lists)
+
+    while True:
+        lenS = len(lists) #6
+
+        if lenS == 0:
+            return 1
+        elif lenS > 0 and (index == initlenS-1 or index == 0):
+            return 0
+        #elif index == initlenS -1
+
+        else:
+            for i in range(0,lenS-1):
+                if lists[i] == lists[i+1]:
+                    tmp = lists[i]
+                    lists.remove(tmp)
+                    lists.remove(tmp)
+                    index += i+1 #4
+                    break;
+
+if __name__ == "__main__":
+    s = "baabaa"
+
+    print(solution(s))
