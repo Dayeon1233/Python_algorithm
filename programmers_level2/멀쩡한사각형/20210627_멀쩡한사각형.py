96f74153f31c75220a3d92dea7338b79ea2d06ca
dayeon <swdy1233@gmail.com>
2021-06-27 20:02:02 +0900

    20210627_멀쩡한사각형

--- programmers_level2/멀쩡한사각형/20210627_멀쩡한사각형.py
+++ programmers_level2/멀쩡한사각형/20210627_멀쩡한사각형.py
@@ -1,30 +1,17 @@
-def solution(record):
-    answer = []
-    nameDict = {}
-    for i in record :
-        tmp = i.split()
-        if i.startswith("E"):
-            id = tmp[1]
-            nameDict[id]=tmp[2]
-
-        elif i.startswith("C"):
-            nameDict[tmp[1]] = tmp[2]
-
-    for i in record:
-        tmp = i.split()
-        if i.startswith("E"):
-            nickname = nameDict[tmp[1]]
-            answer.append(nickname+"님이 들어왔습니다.")
+import math
 
 
-        elif i.startswith("L"):
-            nickname = nameDict[tmp[1]]
-            answer.append(nickname + "님이 나갔습니다.")
+def solution(w,h):
+    gcd = math.gcd(w,h)
+    naw = w// gcd
+    nah = h//gcd
 
+    return w*h - (naw + nah -1) * gcd
 
-    return answer
+    #return w*h-w-h+gcd(w,h)
 
-
+# 8 12 4
 if __name__ == "__main__":
-    record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
-    print(solution(record))
+    W = 8
+    H = 12
+    print(solution(W,H))
